44d0f7dbfb3ff621bdf2d9b77b53e12534b66406
David Daza <03dazal@gmail.com>
2021-05-13 21:16:10 -0500

    add mock content to faq page

--- ckanext/pdae_theme/plugin.py
+++ ckanext/pdae_theme/plugin.py
@@ -1,14 +1,31 @@
+# import routes.mapper
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from flask import Blueprint, render_template
+
+
+def faq():
+    return render_template('faq.html')
 
 
 class PdaeThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-
-    # IConfigurer
+    plugins.implements(plugins.IBlueprint)
 
     def update_config(self, config_):
+        print(vars(plugins.IRoutes.after_map))
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
-            'pdae_theme')
+                             'pdae_theme')
+
+    def get_blueprint(self):
+        blueprint = Blueprint(self.name, self.__module__)
+        blueprint.template_folder = 'templates'
+        rules = [
+            ('/faq', 'faq', faq)
+        ]
+        for rule in rules:
+            blueprint.add_url_rule(*rule)
+        return blueprint
